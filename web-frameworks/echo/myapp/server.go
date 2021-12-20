348f18f957685e61bcaea6aa39ec5feb40370756
nibeon <nibeon@gmail.com>
2021-12-20 14:55:25 +0200

    see web-frameworks gin, beego and echo
    Signed-off-by: nibeon <nibeon@gmail.com>

---
+++ web-frameworks/echo/myapp/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo/v4"
+)
+
+func main() {
+    e := echo.New()
+    
+    e.POST("/save", save)
+    
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!")
+    })
+    e.GET("users/:id", getUser)
+    e.GET("/show", show)
+    
+    e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+    id := c.Param("id")
+    return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+    team := c.QueryParam("team")
+    member := c.QueryParam("member")
+    return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func save(c echo.Context) error {
+    // Get name and email
+    name := c.FormValue("name")
+    email := c.FormValue("email")
+    return c.String(http.StatusOK, "name: " + name + ", email: " + email)
+}
